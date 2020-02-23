6f7ba352ff39fbaebce7e7256ef108ff1d12f521
Guilherme Ferreira <guilhermef18@gmail.com>
2020-02-23 21:04:34 +0100

    added pdf i18n support

--- src/build.js
+++ src/build.js
@@ -15,18 +15,16 @@ fs.emptyDirSync(outputDir);
 // Copy assets
 fs.copySync(srcDir + '/assets', outputDir);
 fs.copySync(`${srcDir}/../node_modules/dom-i18n/dist`, `${outputDir}/js`)
+fs.ensureDirSync(`${outputDir}/pdf`)
 
-// Build HTML
-handlebars.registerHelper('markdown', markdownHelper);
-const source = fs.readFileSync(srcDir + '/templates/index.html', 'utf-8');
-const template = handlebars.compile(source);
-const pdfFileName = `${getSlug(templateData.name)}.${getSlug(templateData.title)}.pdf`;
-const html = template({
+getPdfName = lang => `pdf/${getSlug(templateData.name)}_${lang}.pdf`;
+
+buildHtml = (pdf, lang, dateFormat) => template({
   ...templateData,
-  pdfFileName,
-  updated: dayjs().format('MMMM D, YYYY'),
+  pdf,
+  language: lang,
+  updated: dayjs().format(dateFormat),
 });
-fs.writeFileSync(outputDir + '/index.html', html);
 
 buildPdf = async function (inputFile, outputFile) {
   const browser = await Puppeteer.launch();
@@ -48,5 +46,29 @@ buildPdf = async function (inputFile, outputFile) {
   await browser.close();
 };
 
+buildPdfSync = async (inputFile, outputFile) => {
+  await buildPdf(inputFile, outputFile)
+  fs.removeSync(inputFile);
+}
+
+// Build HTML
+handlebars.registerHelper('markdown', markdownHelper);
+const source = fs.readFileSync(srcDir + '/templates/index.html', 'utf-8');
+const template = handlebars.compile(source);
+const pdf = {
+  en: getPdfName('en'),
+  fr: getPdfName('fr'),
+  pt: getPdfName('pt')
+}
+const html = buildHtml(pdf, 'en', 'MMMM D, YYYY');
+fs.writeFileSync(outputDir + '/index.html', html);
 // Build PDF
-buildPdf(`${outputDir}/index.html`, `${outputDir}/${pdfFileName}`);
+buildPdf(`${outputDir}/index.html`, `${outputDir}/${pdf.en}`);
+
+const frHtml = buildHtml(pdf, 'fr', 'DD/MM/YYYY');
+fs.writeFileSync(`${outputDir}/index.fr.html`, frHtml);
+buildPdfSync(`${outputDir}/index.fr.html`, `${outputDir}/${pdf.fr}`);
+
+const ptHtml = buildHtml(pdf, 'pt', 'DD/MM/YYYY');
+fs.writeFileSync(`${outputDir}/index.pt.html`, ptHtml);
+buildPdfSync(`${outputDir}/index.pt.html`, `${outputDir}/${pdf.pt}`);
